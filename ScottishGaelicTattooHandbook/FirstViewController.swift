ded2a677bde213895e1bd966eb922180aab355ac
Chelsea Troy <chelsea.dommert@gmail.com>
2020-06-27 18:28:58 -0500

    Initial Commit

---
+++ ScottishGaelicTattooHandbook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ScottishGaelicTattooHandbook
+//
+//  Created by Chelsea Troy on 6/27/20.
+//  Copyright © 2020 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScottishGaelicTattooHandbook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ScottishGaelicTattooHandbook
+//
+//  Created by Chelsea Troy on 6/27/20.
+//  Copyright © 2020 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
