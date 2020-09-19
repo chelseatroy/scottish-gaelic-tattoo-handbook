f8a103934cb1308ca607fc340cce4a4fd8a9ba47
Chelsea Troy <chelsea.dommert@gmail.com>
2020-09-19 16:10:01 -0500

    Replace custom options for rating the app with the official Apple review request AlertDialog

--- ScottishGaelicTattooHandbook/MenuViewController.swift
+++ ScottishGaelicTattooHandbook/MenuViewController.swift
@@ -7,32 +7,11 @@
 //
 
 import UIKit
+import StoreKit
 
 class MenuViewController: UIViewController {
 
     @IBAction func didTapToRate(_ sender: Any) {
-        
-        let alert = UIAlertController(
-            title: "Rate us!",
-            message: "Enjoying the Scottish Gaelic Tattoo Handbook? Please take a moment to rate it in the app store!",
-            preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(
-            title: "Take me to the app store",
-            style: .default) {
-                (action: UIAlertAction!) in
-                if let url = URL(string: "https://www.apple.com/ios/app-store/") {
-                    UIApplication.shared.open(url)
-                }
-        })
-        
-        alert.addAction(UIAlertAction(
-            title: "Cancel",
-            style: .cancel,
-            handler: nil
-        ))
-        
-        self.present(alert, animated: true)
+        SKStoreReviewController.requestReview()
     }
-    
 }
